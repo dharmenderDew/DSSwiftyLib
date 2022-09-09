16be2e9083029e512ea3287bf8164b6971384d89
dharmenderDew <113166286+dharmenderDew@users.noreply.github.com>
2022-09-09 16:08:41 +0530

    Added test cases

---
+++ DSSwiftyLib/DSSwiftyLib.swift
@@ -0,0 +1,22 @@
+//
+//  DSSwiftyLib.swift
+//  DSSwiftyLib
+//
+//  Created by Dharmender Singh on 09/09/22.
+//
+
+import Foundation
+
+public final class DSSwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
