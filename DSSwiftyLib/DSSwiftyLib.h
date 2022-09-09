78631ab7265a495177eed33f78b06171b17c8acd
dharmenderDew <113166286+dharmenderDew@users.noreply.github.com>
2022-09-09 14:58:07 +0530

    Initial Upload

---
+++ DSSwiftyLib/DSSwiftyLib.h
@@ -0,0 +1,18 @@
+//
+//  DSSwiftyLib.h
+//  DSSwiftyLib
+//
+//  Created by Dharmender Singh on 09/09/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DSSwiftyLib.
+FOUNDATION_EXPORT double DSSwiftyLibVersionNumber;
+
+//! Project version string for DSSwiftyLib.
+FOUNDATION_EXPORT const unsigned char DSSwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSSwiftyLib/PublicHeader.h>
+
+
---
+++ LibraryExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LibraryExamples
+//
+//  Created by Dharmender Singh on 09/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
